2fc44e536d65f021503566e9f140029be6ef025a
Nataly Villafañez <139076326+villafaneznat@users.noreply.github.com>
2024-06-24 13:21:25 -0300

    Actualizacion: Repositorio y servicios de Size

---
+++ TP01EF2024.Datos/Repositorios/SizeRepository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP01EF2024.Datos.Interfaces;
+using TP01EF2024.Entidades;
+using TP01EF2024.Entidades.Enums;
+
+namespace TP01EF2024.Datos.Repositorios
+{
+    public class SizeRepository : ISizesRepository
+    {
+        private readonly TP01DbContext _context;
+
+        public SizeRepository(TP01DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Agregar(Size size)
+        {
+            _context.Sizes.Add(size);
+        }
+
+        public void Editar(Size size)
+        {
+            _context.Sizes.Update(size);
+        }
+
+        public void Eliminar(Size size)
+        {
+            _context.Sizes.Remove(size);
+        }
+
+        public bool EstaRelacionado(Size size)
+        {
+            return _context.ShoesSizes.Any(ss => ss.SizeId == size.SizeId);
+        }
+
+        public bool Existe(Size size)
+        {
+            if (size.SizeId == 0)
+            {
+                return _context.Sizes.Any(s => s.SizeNumber == size.SizeNumber);
+            }
+            return _context.Sizes.Any(s => s.SizeNumber == size.SizeNumber && s.SizeId != size.SizeId);
+        }
+
+        public int GetCantidad()
+        {
+            return _context.Sizes.Count();
+        }
+
+        public Size? GetSizePorId(int id)
+        {
+            return _context.Sizes.SingleOrDefault(s => s.SizeId == id);
+        }
+
+        public List<Size> GetSizes()
+        {
+            return _context.Sizes.AsNoTracking().ToList();
+        }
+
+        public List<Size> GetSizesPaginadosOrdenados(int page, int pageSize, Orden? orden = null)
+        {
+            IQueryable<Size> query = _context.Sizes.AsNoTracking();
+
+            //ORDEN
+            if (orden != null)
+            {
+                switch (orden)
+                {
+                    case Orden.AZ:
+                        query = query.OrderBy(s => s.SizeNumber);
+                        break;
+                    case Orden.ZA:
+                        query = query.OrderByDescending(s => s.SizeNumber);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //PAGINADO
+            List<Size> listaPaginada = query.AsNoTracking()
+                .Skip(page * pageSize) //Saltea estos registros
+                .Take(pageSize) //Muestra estos
+                .ToList();
+
+            return listaPaginada;
+        }
+    }
+}
